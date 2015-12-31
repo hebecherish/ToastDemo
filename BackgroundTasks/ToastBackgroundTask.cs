c639953378a87209b979afffa73e251b0bc8eff7
mingqun zhang <zmq_cherish@outlook.com>
2015-12-31 14:25:20 +0800

    Toast Demo

---
+++ BackgroundTasks/ToastBackgroundTask.cs
@@ -0,0 +1,26 @@
+﻿using Windows.ApplicationModel.Background;
+using Windows.Storage;
+using Windows.UI.Notifications;
+
+namespace BackgroundTasks
+{
+    public sealed class ToastBackgroundTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var deferral = taskInstance.GetDeferral();
+            var details = taskInstance.TriggerDetails as ToastNotificationActionTriggerDetail;
+            if (details != null)
+            {
+                string arg = details.Argument;
+                if (arg == "ok")//判断用户是否点击ok
+                {
+                    // 保存数据
+                    var settings = ApplicationData.Current.LocalSettings;
+                    settings.Values["name"] = details.UserInput["name"];// 获取选择的项
+                }
+            }
+            deferral.Complete();
+        }
+    }
+}
---
+++ ToastDemo/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Windows.Data.Xml.Dom;
+using Windows.Storage;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace ToastDemo
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //string xml = "<toast scenario=\"reminder\">" +
+            //              "<visual>" +
+            //                  "<binding template=\"ToastGeneric\">" +
+            //                      "<text>Title</text>" +
+            //                      "<image placement=\"inline\" src=\"ms-appx:///Assets/1.jpg\" />" +
+            //                  "</binding>" +
+            //              "</visual>" +
+            //           "</toast>";
+            //ShowToast(xml);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var settings = ApplicationData.Current.LocalSettings;
+            var val = settings.Values["name"];
+            if (null!=val)
+            {
+                string name = val.ToString();
+                //TODO
+                ShowToastArgs(name);
+            }
+            base.OnNavigatedTo(e);
+        }
+
+        public void ShowToastArgs(string arg)
+        {
+            txtArg.Text = arg;
+        }
+
+        private void foregroundToast_Click(object sender, RoutedEventArgs e)
+        {
+            string xml = "<toast>" +
+                          "<visual>" +
+                              "<binding template=\"ToastGeneric\">" +
+                                  "<text>Title</text>" +
+                              "</binding>" +
+                          "</visual>" +
+                          "<actions>" +
+                                  "<input id=\"name\" type=\"text\" placeHolderContent=\"请输入姓名\" />" +
+                                  "<action content = \"确定\" arguments = \"ok\" activationType=\"foreground\"/>" +
+                                  "<action content = \"取消\" arguments = \"cancel\" activationType=\"foreground\"/>" +
+                          "</actions >" +
+                       "</toast>";
+            ShowToast(xml);
+        }
+
+        private void backgroundToast_Click(object sender, RoutedEventArgs e)
+        {
+            string xml = "<toast>" +
+                        "<visual>" +
+                            "<binding template=\"ToastGeneric\">" +
+                                "<text>Title</text>" +
+                            "</binding>" +
+                        "</visual>" +
+                        "<actions>" +
+                                "<input id=\"name\" type=\"text\" placeHolderContent=\"请输入姓名\" />" +
+                                "<action content = \"确定\" arguments = \"ok\" activationType=\"background\"/>" +
+                                "<action content = \"取消\" arguments = \"cancel\" activationType=\"background\"/>" +
+                        "</actions >" +
+                     "</toast>";
+            ShowToast(xml);
+        }
+
+        private static void ShowToast(string xml)
+        {
+            // 创建XML文档
+            XmlDocument doc = new XmlDocument();
+            // 加载XML
+            doc.LoadXml(xml);
+            // 创建通知实例
+            ToastNotification notification = new ToastNotification(doc);
+            // 显示通知
+            ToastNotifier nt = ToastNotificationManager.CreateToastNotifier();
+            nt.Show(notification);
+        }
+    }
+}
